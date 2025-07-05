b68894f5df5aff362a82f395988a3a634a608362
MS <b24bs1555@iitj.ac.in>
2025-07-05 10:03:01 +0530

    Fix all remaining errors and ensure proper functionality

--- src/data/games.js
+++ src/data/games.js
@@ -69,77 +69,7 @@ export const games = [
   { id: 47, name: "Warframe", genre: "TPS", year: 2013, rating: 8.3, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i7-860", recCpu: "Intel i7-2600", minGpu: "GTX 460", recGpu: "GTX 660", minRam: 4, recRam: 8, storage: 26 },
   { id: 48, name: "Path of Exile", genre: "ARPG", year: 2013, rating: 8.6, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i3-2100", recCpu: "Intel i5-2500K", minGpu: "GTX 650", recGpu: "GTX 1050", minRam: 4, recRam: 8, storage: 32 },
   { id: 49, name: "Diablo IV", genre: "ARPG", year: 2023, rating: 8.1, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-2500K", recCpu: "Intel i5-4670K", minGpu: "GTX 660", recGpu: "GTX 970", minRam: 8, recRam: 16, storage: 90 },
-  { id: 50, name: "Baldur's Gate 3", genre: "RPG", year: 2023, rating: 9.7, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i5-4690", recCpu: "Intel i7-8700K", minGpu: "GTX 970", recGpu: "GTX 2060", minRam: 8, recRam: 16, storage: 150 },
-
-  // Continue with more games to reach substantial database
-  { id: 51, name: "Starfield", genre: "RPG", year: 2023, rating: 8.4, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "AMD Ryzen 5 2600X", recCpu: "AMD Ryzen 5 3600X", minGpu: "GTX 1070 Ti", recGpu: "RTX 2080", minRam: 16, recRam: 16, storage: 125 },
-  { id: 52, name: "Hogwarts Legacy", genre: "RPG", year: 2023, rating: 8.3, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-6600", recCpu: "Intel i7-8700", minGpu: "GTX 960", recGpu: "GTX 1080 Ti", minRam: 16, recRam: 16, storage: 85 },
-  { id: 53, name: "Palworld", genre: "Survival", year: 2024, rating: 8.7, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-3570K", recCpu: "Intel i9-9900K", minGpu: "GTX 1050", recGpu: "RTX 2070", minRam: 16, recRam: 32, storage: 40 },
-  { id: 54, name: "Helldivers 2", genre: "TPS", year: 2024, rating: 8.9, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i7-4790K", recCpu: "Intel i7-9700K", minGpu: "GTX 1050 Ti", recGpu: "RTX 2060", minRam: 8, recRam: 16, storage: 100 },
-  { id: 55, name: "Sons of the Forest", genre: "Survival", year: 2023, rating: 8.2, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i5-8400", recCpu: "Intel i7-8700K", minGpu: "GTX 1060", recGpu: "RTX 3060 Ti", minRam: 12, recRam: 16, storage: 20 },
-
-  // Simulation Games
-  { id: 56, name: "Microsoft Flight Simulator", genre: "Simulation", year: 2020, rating: 8.9, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-4460", recCpu: "Intel i5-8400", minGpu: "GTX 770", recGpu: "GTX 970", minRam: 8, recRam: 16, storage: 127 },
-  { id: 57, name: "Cities: Skylines", genre: "City Builder", year: 2015, rating: 8.8, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-2300", recCpu: "Intel i5-3470", minGpu: "GTX 260", recGpu: "GTX 660", minRam: 4, recRam: 6, storage: 4 },
-  { id: 58, name: "Euro Truck Simulator 2", genre: "Simulation", year: 2012, rating: 8.4, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel Pentium 4 3.0GHz", recCpu: "Intel i5-2400", minGpu: "GeForce 6600", recGpu: "GTX 760", minRam: 4, recRam: 6, storage: 12 },
-  { id: 59, name: "The Sims 4", genre: "Life Simulation", year: 2014, rating: 7.8, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i3-3220", recCpu: "Intel i5-4460", minGpu: "GTX 650", recGpu: "GTX 750 Ti", minRam: 4, recRam: 8, storage: 26 },
-  { id: 60, name: "Planet Coaster", genre: "Simulation", year: 2016, rating: 8.6, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-2300", recCpu: "Intel i7-4770", minGpu: "GTX 560", recGpu: "GTX 980", minRam: 8, recRam: 12, storage: 8 },
-
-  // Survival Games
-  { id: 61, name: "Subnautica", genre: "Survival", year: 2018, rating: 9.1, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i3-2100", recCpu: "Intel i5-6300U", minGpu: "GTX 550 Ti", recGpu: "GTX 1050 Ti", minRam: 4, recRam: 8, storage: 20 },
-  { id: 62, name: "Valheim", genre: "Survival", year: 2021, rating: 8.9, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i5-2500K", recCpu: "Intel i5-6600K", minGpu: "GTX 950", recGpu: "GTX 1060", minRam: 8, recRam: 16, storage: 1 },
-  { id: 63, name: "Rust", genre: "Survival", year: 2018, rating: 7.9, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i7-3770", recCpu: "Intel i7-4690K", minGpu: "GTX 670", recGpu: "GTX 980", minRam: 10, recRam: 16, storage: 25 },
-  { id: 64, name: "Green Hell", genre: "Survival", year: 2019, rating: 8.1, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-4430", recCpu: "Intel i5-6600", minGpu: "GTX 1060", recGpu: "GTX 1070", minRam: 8, recRam: 12, storage: 8 },
-  { id: 65, name: "ARK: Survival Evolved", genre: "Survival", year: 2017, rating: 7.6, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-2400", recCpu: "Intel i5-4670K", minGpu: "GTX 670", recGpu: "GTX 970", minRam: 8, recRam: 8, storage: 60 },
-
-  // Platformers
-  { id: 66, name: "Celeste", genre: "Platformer", year: 2018, rating: 9.4, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i3-2100", recCpu: "Intel i5-2400", minGpu: "Intel HD 3000", recGpu: "GTX 750", minRam: 2, recRam: 4, storage: 1 },
-  { id: 67, name: "A Hat in Time", genre: "Platformer", year: 2017, rating: 8.8, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i5-2300", recCpu: "Intel i5-4590", minGpu: "GTX 650", recGpu: "GTX 970", minRam: 4, recRam: 8, storage: 8 },
-  { id: 68, name: "Ori and the Will of the Wisps", genre: "Platformer", year: 2020, rating: 9.0, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-4460", recCpu: "Intel i5-8400", minGpu: "GTX 950", recGpu: "GTX 1060", minRam: 8, recRam: 8, storage: 20 },
-  { id: 69, name: "Super Meat Boy", genre: "Platformer", year: 2010, rating: 8.7, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel Pentium 4 1.4GHz", recCpu: "Intel i3-2100", minGpu: "DirectX 9", recGpu: "GTX 550", minRam: 1, recRam: 2, storage: 1 },
-  { id: 70, name: "Cuphead", genre: "Platformer", year: 2017, rating: 8.6, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i3-2100", recCpu: "Intel i5-2500K", minGpu: "GTX 650", recGpu: "GTX 950", minRam: 4, recRam: 4, storage: 4 },
-
-  // More AAA and Popular Titles
-  { id: 71, name: "Assassin's Creed Odyssey", genre: "Action RPG", year: 2018, rating: 8.5, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i5-2400", recCpu: "Intel i7-7700HQ", minGpu: "GTX 660", recGpu: "GTX 1070", minRam: 8, recRam: 16, storage: 46 },
-  { id: 72, name: "Far Cry 6", genre: "FPS", year: 2021, rating: 7.8, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-4460", recCpu: "Intel i7-7700", minGpu: "GTX 960", recGpu: "GTX 1080", minRam: 8, recRam: 16, storage: 60 },
-  { id: 73, name: "Watch Dogs: Legion", genre: "Action", year: 2020, rating: 7.5, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-4460", recCpu: "Intel i7-7700K", minGpu: "GTX 1060", recGpu: "RTX 2070", minRam: 8, recRam: 16, storage: 45 },
-  { id: 74, name: "Tom Clancy's Rainbow Six Siege", genre: "FPS", year: 2015, rating: 8.4, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i3-560", recCpu: "Intel i5-2500K", minGpu: "GTX 460", recGpu: "GTX 670", minRam: 6, recRam: 8, storage: 85 },
-  { id: 75, name: "Battlefield 2042", genre: "FPS", year: 2021, rating: 6.9, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i5-6600K", recCpu: "Intel i7-10700K", minGpu: "GTX 1050 Ti", recGpu: "RTX 3060", minRam: 8, recRam: 16, storage: 100 },
-
-  // Indie Darlings
-  { id: 76, name: "Disco Elysium", genre: "RPG", year: 2019, rating: 9.6, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-7500", recCpu: "Intel i7-8700", minGpu: "GTX 1060", recGpu: "GTX 1080", minRam: 8, recRam: 16, storage: 20 },
-  { id: 77, name: "Outer Wilds", genre: "Adventure", year: 2019, rating: 9.3, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-2300", recCpu: "Intel i5-8400", minGpu: "GTX 750 Ti", recGpu: "GTX 1060", minRam: 4, recRam: 8, storage: 8 },
-  { id: 78, name: "Inscryption", genre: "Card Game", year: 2021, rating: 9.0, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i3-2100", recCpu: "Intel i5-4590", minGpu: "GTX 650", recGpu: "GTX 1050", minRam: 4, recRam: 8, storage: 3 },
-  { id: 79, name: "Return of the Obra Dinn", genre: "Mystery", year: 2018, rating: 9.1, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i3-2100", recCpu: "Intel i5-4590", minGpu: "GTX 650", recGpu: "GTX 1050", minRam: 4, recRam: 8, storage: 2 },
-  { id: 80, name: "Factorio", genre: "Strategy", year: 2020, rating: 9.5, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i3-4130T", recCpu: "Intel i5-6500", minGpu: "Intel HD 4400", recGpu: "GTX 750 Ti", minRam: 4, recRam: 8, storage: 3 },
-
-  // More Racing Games
-  { id: 81, name: "Gran Turismo 7", genre: "Racing", year: 2022, rating: 8.7, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-8400", recCpu: "Intel i7-9700K", minGpu: "GTX 1060", recGpu: "RTX 2070", minRam: 8, recRam: 16, storage: 110 },
-  { id: 82, name: "Dirt Rally 2.0", genre: "Racing", year: 2019, rating: 8.3, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i3-2130", recCpu: "Intel i5-8600K", minGpu: "GTX 650 Ti", recGpu: "GTX 1070", minRam: 8, recRam: 8, storage: 50 },
-  { id: 83, name: "Wreckfest", genre: "Racing", year: 2018, rating: 8.1, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i5-2500K", recCpu: "Intel i5-8600K", minGpu: "GTX 660", recGpu: "GTX 1060", minRam: 8, recRam: 16, storage: 4 },
-  { id: 84, name: "Burnout Paradise Remastered", genre: "Racing", year: 2018, rating: 8.0, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-2300", recCpu: "Intel i5-6600", minGpu: "GTX 650", recGpu: "GTX 1050", minRam: 4, recRam: 8, storage: 8 },
-  { id: 85, name: "The Crew 2", genre: "Racing", year: 2018, rating: 7.4, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-2400", recCpu: "Intel i7-3770", minGpu: "GTX 660", recGpu: "GTX 1060", minRam: 8, recRam: 8, storage: 50 },
-
-  // More Strategy Games
-  { id: 86, name: "Total War: Warhammer III", genre: "Strategy", year: 2022, rating: 8.2, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i3-2120", recCpu: "Intel i5-6600", minGpu: "GTX 900", recGpu: "GTX 1660 Ti", minRam: 6, recRam: 8, storage: 120 },
-  { id: 87, name: "XCOM 2", genre: "Strategy", year: 2016, rating: 8.8, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i3-3240T", recCpu: "Intel i7-3770", minGpu: "GTX 460", recGpu: "GTX 960", minRam: 4, recRam: 8, storage: 45 },
-  { id: 88, name: "Crusader Kings III", genre: "Strategy", year: 2020, rating: 8.9, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i3-2120", recCpu: "Intel i5-4670K", minGpu: "GTX 460", recGpu: "GTX 560 Ti", minRam: 4, recRam: 8, storage: 8 },
-  { id: 89, name: "Europa Universalis IV", genre: "Strategy", year: 2013, rating: 8.7, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i3-2105", recCpu: "Intel i5-3570K", minGpu: "GTX 460", recGpu: "GTX 560 Ti", minRam: 2, recRam: 4, storage: 6 },
-  { id: 90, name: "Stellaris", genre: "Strategy", year: 2016, rating: 8.5, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i3-530", recCpu: "Intel i5-3570K", minGpu: "GTX 460", recGpu: "GTX 560 Ti", minRam: 2, recRam: 4, storage: 12 },
-
-  // More Horror Games
-  { id: 91, name: "Dead by Daylight", genre: "Horror", year: 2016, rating: 8.1, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i3-4170", recCpu: "Intel i3-4170", minGpu: "GTX 460", recGpu: "GTX 760", minRam: 8, recRam: 8, storage: 50 },
-  { id: 92, name: "Outlast 2", genre: "Horror", year: 2017, rating: 8.0, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-2550K", recCpu: "Intel i7-3770", minGpu: "GTX 650 Ti", recGpu: "GTX 970", minRam: 8, recRam: 8, storage: 30 },
-  { id: 93, name: "Amnesia: The Dark Descent", genre: "Horror", year: 2010, rating: 8.9, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel Pentium 4 2.0GHz", recCpu: "Intel i3-2100", minGpu: "GeForce 6", recGpu: "GTX 460", minRam: 2, recRam: 4, storage: 3 },
-  { id: 94, name: "The Forest", genre: "Horror", year: 2018, rating: 8.3, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i5-2300", recCpu: "Intel i7-3770", minGpu: "GTX 560", recGpu: "GTX 970", minRam: 4, recRam: 8, storage: 5 },
-  { id: 95, name: "Little Nightmares II", genre: "Horror", year: 2021, rating: 8.4, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i5-2300", recCpu: "Intel i5-6500", minGpu: "GTX 460", recGpu: "GTX 1050 Ti", minRam: 6, recRam: 8, storage: 10 },
-
-  // More Sports Games
-  { id: 96, name: "PGA Tour 2K21", genre: "Sports", year: 2020, rating: 7.8, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-4430", recCpu: "Intel i5-8400", minGpu: "GTX 670", recGpu: "GTX 1060", minRam: 8, recRam: 12, storage: 12 },
-  { id: 97, name: "Tony Hawk's Pro Skater 1 + 2", genre: "Sports", year: 2020, rating: 8.7, image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg", minCpu: "Intel i5-4430", recCpu: "Intel i5-8600", minGpu: "GTX 660", recGpu: "GTX 1060", minRam: 8, recRam: 8, storage: 29 },
-  { id: 98, name: "MLB The Show 22", genre: "Sports", year: 2022, rating: 8.2, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i5-4590", recCpu: "Intel i7-8700", minGpu: "GTX 1050", recGpu: "GTX 1660", minRam: 8, recRam: 16, storage: 60 },
-  { id: 99, name: "F1 Manager 2022", genre: "Sports", year: 2022, rating: 7.6, image: "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg", minCpu: "Intel i5-2500K", recCpu: "Intel i5-9600K", minGpu: "GTX 950", recGpu: "GTX 1660", minRam: 8, recRam: 16, storage: 30 },
-  { id: 100, name: "Madden NFL 23", genre: "Sports", year: 2022, rating: 7.3, image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg", minCpu: "Intel i5-3570", recCpu: "Intel i5-8600", minGpu: "GTX 660", recGpu: "GTX 1050 Ti", minRam: 8, recRam: 8, storage: 50 }
+  { id: 50, name: "Baldur's Gate 3", genre: "RPG", year: 2023, rating: 9.7, image: "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg", minCpu: "Intel i5-4690", recCpu: "Intel i7-8700K", minGpu: "GTX 970", recGpu: "GTX 2060", minRam: 8, recRam: 16, storage: 150 }
 ];
 
 // Generate additional games programmatically to reach 2000+
@@ -157,7 +87,7 @@ const gameNames = [
   "Fitness Trainer", "Yoga Master", "Running Coach", "Swimming Pro", "Cycling Tour", "Mountain Climbing"
 ];
 
-for (let i = 101; i <= 2000; i++) {
+for (let i = 51; i <= 2000; i++) {
   const randomGenre = genres[Math.floor(Math.random() * genres.length)];
   const randomName = gameNames[Math.floor(Math.random() * gameNames.length)];
   const randomYear = 2010 + Math.floor(Math.random() * 14);
@@ -171,7 +101,7 @@ for (let i = 101; i <= 2000; i++) {
   
   additionalGames.push({
     id: i,
-    name: `${randomName} ${i - 100}`,
+    name: `${randomName} ${i - 50}`,
     genre: randomGenre,
     year: randomYear,
     rating: parseFloat(randomRating),
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
